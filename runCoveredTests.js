b988cab67cc6f84fb7cee453655090e868d208e7
maurelian <leighvert@protonmail.ch>
2017-03-26 17:36:14 -0400

    Changes to run testrpc outside of this file

--- runCoveredTests.js
+++ runCoveredTests.js
@@ -17,14 +17,17 @@ if (!shell.test('-e', './node_modules/ethereumjs-vm/lib/opFns.js.orig')) {
   shell.exec('patch -b ./node_modules/ethereumjs-vm/lib/opFns.js ./hookIntoEvents.patch');
 }
 // Run the modified testrpc with large block limit
-const testrpcProcess = childprocess.exec('./node_modules/ethereumjs-testrpc/bin/testrpc --gasLimit 0xfffffffffffff --gasPrice 0x1');
+// commen this out and just run the same in another terminal. 
+// const testrpcProcess = childprocess.exec('./node_modules/ethereumjs-testrpc/bin/testrpc --gasLimit 0xfffffffffffff --gasPrice 0x1 --accounts 25');
 
 if (shell.test('-d', '../originalContracts')) {
+  shell.rm('-rf', './../contracts');
+  shell.mv('./../originalContracts', './../contracts');
   console.log('There is already an "originalContracts" directory in your truffle directory.\n' +
               'This is probably due to a previous solcover failure.\n' +
               'Please make sure the ./contracts/ directory contains your contracts (perhaps by copying them from originalContracts), ' +
               'and then delete the originalContracts directory.');
-  process.exit(1);
+  // process.exit(1);
 }
 
 shell.mv('./../contracts/', './../originalContracts/');
@@ -45,7 +48,11 @@ shell.ls('./../originalContracts/**/*.sol').forEach(file => {
 shell.cp('./../originalContracts/Migrations.sol', './../contracts/Migrations.sol');
 
 shell.rm('./allFiredEvents'); // Delete previous results
-shell.exec('truffle test --network test');
+console.log('running `truffle test --network test`')
+
+// shell.exec('truffle test --network test --verbose-rpc');
+shell.exec('./node_modules/truffle/cli.js test --network test');
+
 
 const events = fs.readFileSync('./allFiredEvents').toString().split('\n');
 events.pop();
@@ -56,6 +63,6 @@ coverage.generate(events, './../originalContracts/');
 fs.writeFileSync('./coverage.json', JSON.stringify(coverage.coverage));
 
 shell.exec('./node_modules/istanbul/lib/cli.js report lcov');
-testrpcProcess.kill();
+// testrpcProcess.kill();
 shell.rm('-rf', './../contracts');
 shell.mv('./../originalContracts', './../contracts');
